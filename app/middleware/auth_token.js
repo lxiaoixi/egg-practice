039c0c9a181b7adb2e387660ee58c91bb7c0c42b
xiaoxi <494273025@qq.com>
2019-02-21 16:18:29 +0800

    fix(auth_token.js):catch the token expire error

--- app/middleware/auth_token.js
+++ app/middleware/auth_token.js
@@ -2,14 +2,20 @@ module.exports = () => {
   return async function authToken(ctx, next) {
     const { helper, logger, app } = ctx;
     const { config } = app;
-    const token = ctx.get('x-auth-token');
-    logger.info('x-auth-token is', token);
-    if (!token) {
-      return helper.response(ctx, 401, 'token is invalid', '111111');
+
+    try {
+      const token = ctx.get('x-auth-token');
+      logger.info('x-auth-token is', token);
+      if (!token) {
+        return helper.response(ctx, 401, 'token is invalid', '111111');
+      }
+      const payload = await helper.verify(token, config.jwt.secret);
+      logger.info('token decode is', payload);
+      ctx.state.user = payload.id;
+      await next();
+    } catch (e) {
+      logger.info('token auth error', e);
+      return helper.response(ctx, 401, 'token is expired', '111111');
     }
-    const payload = await helper.verify(token, config.jwt.secret);
-    logger.info('token decode is', payload);
-    ctx.state.user = payload.id;
-    await next();
   };
 };
